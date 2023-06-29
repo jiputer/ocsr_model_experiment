50c46be4dba00b72bbf168db3db05492b2861e26
John Ma <jpaulma23@gmail.com>
2023-06-29 16:21:38 -0400

    fixed some parsing problems, created encode and decode functions for the chemicals

--- OCR_chem.py
+++ OCR_chem.py
@@ -10,14 +10,21 @@ import jax.numpy as jnp
 from jax import grad, jit, vmap, random, image
 import matplotlib.pyplot as plt
 
+torch.set_printoptions(threshold=10_000)
 
 # %%
-# parsing the input with the labels
-# make other samples of the input to train with (how should we do this)
-# setting the model up
+
+hsize = 0
+basewidth = 0
 chems = [ "H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","K","Ar","Ca","Sc","Ti","V","Cr","Mn","Fe","Ni","Co","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","I","Te","Xe","Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og","-","+","=", "#", "/", "\\", "[" , "]", "(", ")", "@", "0", "1", "2","3", "4", "5", "6"]
 chems_dict = {symbol: index for index, symbol in enumerate(chems)}
-# inefficient btw
+def arrayToChem(arr):
+    j = ""
+    # all values in arr must be a int
+    for i in arr:
+        j += chems[i]
+    return j
+
 def chemToArray(chem):
     chem = chem.strip()
     temp = [0] * len(chem)
@@ -28,22 +35,23 @@ def chemToArray(chem):
             temp[a] = chems_dict[k]
 
     return temp
-
+#%%
 def parse():
     # create a matrix for each entry in the image data set
     # how would i be able to resample these images to create a bigger data set?
     files = os.listdir(path='/Users/jp/Documents/projects/OCSR_model/dataset')
     X = []
-    basewidth = 400
+    basewidth = 350
     max_formula_len = 185
 
     for file in files:
         img = Image.open(f'dataset/{file}')
+        img = img.convert(mode="1", dither=Image.Dither.FLOYDSTEINBERG)
         wpercent = (basewidth/float(img.size[0]))
         hsize = int((float(img.size[1])*float(wpercent)))
         img = img.resize((basewidth,hsize), Image.Resampling.LANCZOS)
         transform = transforms.Compose([transforms.PILToTensor()])
-        X.append(transform(img))
+        X.append(transform(img).to(torch.uint8))
     
     labels = pd.read_csv('DECIMER_HDM_Dataset_SMILES.tsv', sep='\t', header=0)
     labels = labels['SMILES'].values
@@ -63,14 +71,21 @@ def parse():
 
 # split the data for training and validation
 X, Y = parse()
-x_train, y_train, x_test, y_test = train_test_split(X, Y, test_size=0.25, random_state=69)
+x_train, y_train, x_test, y_test = train_test_split(X, Y, test_size=0.10, random_state=69)
 
 
 
 
-# %% embeddings (integer look up)
 
+# %% pyotrch'd thing
+model = torch.nn.Sequential(
+          torch.nn.Conv2d(1,20,5),
+          torch.nn.ReLU(),
+          torch.nn.Conv2d(20,64,5),
+          torch.nn.ReLU()
+        )
 
+model(input)
 
 # %% positional encodings???? do we need it? uhhh sure ig.
 
@@ -79,12 +94,6 @@ x_train, y_train, x_test, y_test = train_test_split(X, Y, test_size=0.25, random
 
 # %% training the model
 
-def forward_pass():
-    pass 
+
 
 # %% using the model to predict
-
-# standarized the image to the training size
-
-def predict():
-    pass
